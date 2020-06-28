ff9b79e1652b200917dd2be23bde0beaabc8aab0
Yegeun <ukm0613@gmail.com>
2020-06-28 20:37:52 +0100

    Problem Random to Circle Problems

---
+++ random_to_circle.py
@@ -0,0 +1,17 @@
+'''
+Estimate Pi, given to random variables from 0 to 1
+'''
+from random import random
+
+
+def est_pi(n):
+  point_circle = 0
+  total_point = 0
+  for i in range(n):
+    x = random.uniform(0, 1)
+    y = random.uniform(0, 1)
+    dis = x ** 2 + y ** 2
+    if dis <= 1:
+      point_circle += 1
+    total_point += 1
+  return 4 * point_circle / total_point
